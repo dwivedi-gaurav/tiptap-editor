536c14d4ccd48fe744edaaf6f65173a4ca17265d
gaurav <gdwivedi@chegg.com>
2024-05-22 22:34:17 +0530

    fix navbar image

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -6,7 +6,7 @@ function Navbar() {
       <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
         <div className="relative flex h-16 items-center justify-between">
           <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
-            <div className="hidden sm:ml-6 sm:block">
+            <div className="sm:ml-6 sm:block">
               <div className="flex space-x-4">
                 <Image src="/logo.png" alt="logo" width={250} height={40} />
               </div>
